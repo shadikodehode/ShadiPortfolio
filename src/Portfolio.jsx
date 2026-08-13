975836068ab038a4364eab0554c68a0aee14067b
shadikodehode <shadi.kodehode@gmail.com>
2026-08-13 10:40:02 +0200

    Scaffolding

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,3 +1,15 @@
-export default function Portfolio() {
-  
+import { useState } from "react"
+import { NavBar } from "./components/NavBar.jsx"
+import { VIEWS } from "./views/index.js"
+
+export function Portfolio() {
+  const [currentView, setCurrentView] = useState('landing')
+  const ActiveView = VIEWS[currentView]
+
+  return (
+    <div>
+      <NavBar currentView={currentView} setCurrentView={setCurrentView}/>
+      <ActiveView />
+    </div>
+  )
 }
---
+++ src/views/index.js
@@ -0,0 +1,14 @@
+import { Landing } from './Landing.jsx'
+import { Art } from './Art.jsx'
+import { Contact } from './Contact.jsx'
+import { Projects } from './Projects.jsx'
+import { About } from './About.jsx'
+
+
+export const VIEWS = {
+  landing: Landing,
+  projects: Projects,
+  art: Art,
+  about: About,
+  contact: Contact,
+}
